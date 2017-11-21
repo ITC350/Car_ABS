28a848b89c29bcbebb5a0c9f22012b3c6644c075
Line <lvni16@student.aau.dk>
2017-11-21 09:49:33 +0100

    Har indsat ny funktion for servo

--- servo.cpp
+++ servo.cpp
@@ -8,5 +8,11 @@
 
 #include "servo.hpp"
 
-servo::servo()  {}
+servo::servo()  {
+  TCCR3B = TCCR3B & 0b11111000 | 0x04;
+  pinMode(m_servoPin, OUTPUT);
+}
+void servo::Servo_turn(turn_val dir){
+  analogWrite(m_servoPin, dir);
+}
 servo::~servo() {}
--- servo.hpp
+++ servo.hpp
@@ -13,9 +13,12 @@
 
 class servo {
 private:
+  const int m_servoPin = 5;
+  enum turn_val{LEFT=32, RIGHT=64, HALEFT=40, HARIGHT=56, CENTRUM=48};
 public:
     servo();
     ~servo();
+    void Servo_turn(turn_val dir);
 protected:
 
 };
