f54cd58016f3c11f3d3d33ebd8406aeaf1f0da65
Kasper Steensig <kasper.steensig@gmail.com>
2017-11-23 08:42:11 +0100

    add scheduler to manage the state machine

---
+++ lib/scheduler/opcode.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef enum opcode_e {
+    NOP         = 0x00,
+    START       = 0x01,
+    BRAKE       = 0x02,
+    FORWARD     = 0x03,
+    BACKWARD    = 0x04,
+    TURN        = 0x05,
+    HALT        = 0xFF
+} opcode_t;
