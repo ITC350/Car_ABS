e491d7710d5b666c6438a35a93eef107de109e0a
Jeppe Nellemann <jepnel@gmail.com>
2017-11-27 15:19:40 +0100

    pid

--- lib/dcmotor/dcmotor.hpp
+++ lib/dcmotor/dcmotor.hpp
@@ -9,7 +9,6 @@
 //#include <stdint.h>
 #include <PID_v1.h>
 #include "sensor.hpp"
-#include "communication.hpp"
 
 class dcmotor {
 private:
@@ -21,14 +20,13 @@ private:
   double Kp=2, Ki=5, Kd=0;
   uint16_t dataArr[1000];
   uint16_t dataArrItt = 0;
-  communication m_comm;
 public:
-    dcmotor(communication &comm, uint16_t acc_const, uint8_t acc_to_speed, double kp, double ki, double kd);
+    dcmotor();
     ~dcmotor();
     const uint32_t datafreq = 50;
     void Forward(uint8_t fwd_speed);
     void Backward(uint8_t bwd_speed);
-    void Accelerator(uint8_t acc_to_spd, uint16_t acc_const);
+    void Accelerator();
     void emStop();
     static sensor m_sensors[4];
     void pid();
