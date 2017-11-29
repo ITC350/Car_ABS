9d341bcbea4da89fc4cf62879a03e05363ec0cb2
Unknown <rkraye16@student.aau.dk>
2017-11-29 11:32:33 +0100

    ye

--- lib/servo/servo.cpp
+++ lib/servo/servo.cpp
@@ -10,8 +10,8 @@
 
 servo::servo()  {
   TCCR3B = TCCR3B & 0b11111000 | 0x04;
-  OCR3A = 62500;
-  OCR3B = (62500/2);
+  OCR3A = 62500/250;
+  OCR3B = (62500/500);
   pinMode(m_servoPin, OUTPUT);
 }
 void servo::Servo_turn(turn_val dir){
